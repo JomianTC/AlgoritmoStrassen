162a59777a470be7e29e2ccd87e8ed7bd75d9b07
JomianTC <jomiantoca-2011@hotmail.com>
2021-12-08 09:41:20 -0600

    Modificacion

--- js/scriptFuerzaBruta.js
+++ js/scriptFuerzaBruta.js
@@ -27,6 +27,17 @@ function crearMatrices (tabla, matriz, j){
 
 }
 
+function multiplicaMatrices(matrizA, matrizB, matrizR){
+
+	for (var x = 0; x < entrada.value; x++){
+		for (var y = 0; y < entrada.value; y++){
+			for (var z = 0; z < entrada.value; z++){
+				matrizR[x][y] = matrizR[x][y] + (matrizA[x][z] * matrizB[z][y]);
+			}
+		}
+	}
+}
+
 function iniciarAnimacion(){
 
 	if (entrada.value > 7 || entrada.value < 2) {
@@ -36,40 +47,58 @@ function iniciarAnimacion(){
 	}
 	else{
 
+		//Removemos el boton oculto
 		var divBoton = document.querySelector("#divBoton");
 		divBoton.remove();
 
+		//Removemos el input que recibe el numero
 		var divInput = document.querySelector("#divInput");
 		divInput.remove();
 
+		//Removemos el h2 que tiene el titulo de insertar numeros de filas y columnas
 		var titulo2 = document.querySelector("h2");
 		titulo2.remove();
 
-		entrada.remove();
-		
+		//Creamos la tabla dinamica que sera la que tendra la matriz 1
 		var tabla = document.getElementById("tabla1");
 		
+		//Inicializamos las 3 matrices para isnertar los valores y el resultado
 		var matrizA = [];
 		var matrizB = [];
+		var matrizR = [];
 
+		//Inicializamos las 3 matrices de manera especial
 		for (var w = 0; w < entrada.value; w++) {
 			matrizA[w] = new Array(entrada.value);
 			matrizB[w] = new Array(entrada.value);
+			matrizR[w] = new Array(entrada.value);
 		}
 
+		//Creamos la matriz A con valores aleatorios
 		for (var j = 0; j < entrada.value; j++){
 
 			crearMatrices(tabla, matrizA, j);
 		}
 
+		//Declaramos la tabla 2 donde estara la segudna matriz
 		tabla = document.getElementById("tabla2");
 
+		//Creamos la matriz 2 con los valores aleatorios
 		for (var j = 0; j < entrada.value; j++){
 
 			crearMatrices(tabla, matrizB, j);
 		}
 
-		console.log(matrizA);
-		console.log(matrizB);
+		//Inicializamos la matriz resultado lleno de ceros
+		for (var j = 0; j < entrada.value; j++){
+			for (var i = 0; i < entrada.value; i++){
+
+				matrizR[i][j] = 0;
+			}
+		}
+
+		//Mandamos a llamar la funcion que obtiene el resultado de la 2
+		//matrices creadas
+		multiplicaMatrices(matrizA, matrizB, matrizR);
 	}
 }
